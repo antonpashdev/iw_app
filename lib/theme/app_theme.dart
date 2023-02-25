7bee0b9801e84ab2cb2cb97702da7128a5b8333a
VitkoDmitriy <vitko.dmitriy@gmail.com>
2023-02-25 20:20:21 +0200

    update theme with  appBarTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,35 +2,42 @@ import 'package:flutter/material.dart';
 
 ThemeData getAppTheme() {
   return ThemeData(
-    fontFamily: 'SF Pro Display',
-    colorScheme: const ColorScheme.light(
-      primary: COLOR_ALMOST_BLACK,
-      secondary: COLOR_WHITE,
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        textStyle: const TextStyle(
-          fontSize: 14,
-          fontFamily: 'SF Pro Display',
-          fontWeight: FontWeight.bold,
+      fontFamily: 'SF Pro Display',
+      scaffoldBackgroundColor: COLOR_WHITE,
+      colorScheme: const ColorScheme.light(
+        primary: COLOR_ALMOST_BLACK,
+        secondary: COLOR_WHITE,
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          textStyle: const TextStyle(
+            fontSize: 14,
+            fontFamily: 'SF Pro Display',
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: const EdgeInsets.all(15),
+          textStyle: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            fontFamily: 'SF Pro Display',
+          ),
+          shadowColor: Colors.transparent,
         ),
-        padding: const EdgeInsets.all(15),
-        textStyle: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.w600,
-          fontFamily: 'SF Pro Display',
-        ),
-        shadowColor: Colors.transparent,
       ),
-    ),
-  );
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        iconTheme: IconThemeData(color: COLOR_ALMOST_BLACK),
+        foregroundColor: COLOR_ALMOST_BLACK,
+        backgroundColor: COLOR_WHITE,
+        elevation: 0,
+      ));
 }
 
 const Color COLOR_WHITE = Color(0xffffffff);
