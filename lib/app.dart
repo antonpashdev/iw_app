1a5ffa973cbbb53a888e3914c7acc8e25e5dc883
VitkoDmitriy <vitko.dmitriy@gmail.com>
2023-03-01 07:32:37 +0200

    remove debug banner

--- lib/app.dart
+++ lib/app.dart
@@ -9,6 +9,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Impact Wallet',
       theme: getAppTheme(),
       home: const LoginScreen(),
