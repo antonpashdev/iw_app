f6b2308269a5685a19be421fe08e4f7d422031f5
Anton Pashkovskyi <anton@globalbit.co.il>
2023-03-13 01:22:13 +0200

    add new user version of home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,166 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:iw_app/l10n/generated/app_localizations.dart';
+import 'package:iw_app/theme/app_theme.dart';
+import 'package:iw_app/widgets/list/assets_list_tile.dart';
+import 'package:iw_app/widgets/utils/app_padding.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> organizations = [];
+
+  Widget buildCallToCreateCard(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(width: 20),
+        Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width * 0.6,
+          child: Card(
+            margin: const EdgeInsets.all(0),
+            color: COLOR_LIGHT_GRAY,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(20),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: COLOR_LIGHT_GRAY2,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.all(20),
+                      child: const Icon(
+                        CupertinoIcons.add,
+                        size: 30,
+                        color: COLOR_ALMOST_BLACK,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      AppLocalizations.of(context)!
+                          .homeScreen_createNewOrgTitle,
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    const SizedBox(height: 35),
+                    // ignore: prefer_const_constructors
+                    Text(
+                      AppLocalizations.of(context)!.homeScreen_createNewOrgDesc,
+                      style: const TextStyle(color: COLOR_GRAY),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buildOrganizationCard(BuildContext context, int index) {}
+
+  buildAssetExample() {
+    return AssetsListTile(
+      leading: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          color: COLOR_LIGHT_GRAY2,
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+      name: AppLocalizations.of(context)!.homeScreen_assetsExampleTitle,
+      account: '@orgs_account',
+      tokensAmount: '-',
+      equity: '-',
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final organizationsCards = organizations.isEmpty
+        ? buildCallToCreateCard(context)
+        : ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              ...organizations
+                  .asMap()
+                  .map(
+                    (i, organization) =>
+                        MapEntry(i, buildOrganizationCard(context, i)),
+                  )
+                  .values
+                  .toList(),
+            ],
+          );
+
+    return Scaffold(
+      backgroundColor: COLOR_WHITE,
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          children: [
+            AppPadding(
+              child: Text(
+                '\$0.00',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+            const SizedBox(height: 45),
+            AppPadding(
+              child: Text(
+                AppLocalizations.of(context)!.homeScreen_organizationsTitle,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 290,
+              child: organizationsCards,
+            ),
+            const SizedBox(height: 45),
+            AppPadding(
+              child: Text(
+                AppLocalizations.of(context)!.homeScreen_assetsTitle,
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+            const SizedBox(height: 15),
+            AppPadding(
+              child: buildAssetExample(),
+            ),
+            if (organizations.isEmpty)
+              Column(
+                children: [
+                  const SizedBox(height: 15),
+                  SvgPicture.asset('assets/icons/arrow_up_big.svg'),
+                  const SizedBox(height: 15),
+                  Text(
+                    AppLocalizations.of(context)!.homeScreen_assetsExampleDesc,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,46 +2,77 @@ import 'package:flutter/material.dart';
 
 ThemeData getAppTheme() {
   return ThemeData(
-      fontFamily: 'SF Pro Display',
-      scaffoldBackgroundColor: COLOR_WHITE,
-      colorScheme: const ColorScheme.light(
-        primary: COLOR_ALMOST_BLACK,
-        secondary: COLOR_WHITE,
-      ),
-      textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          textStyle: const TextStyle(
-            fontSize: 14,
-            fontFamily: 'SF Pro Display',
-            fontWeight: FontWeight.bold,
-          ),
+    fontFamily: 'SF Pro Display',
+    scaffoldBackgroundColor: COLOR_WHITE,
+    colorScheme: const ColorScheme.light(
+      primary: COLOR_ALMOST_BLACK,
+      secondary: COLOR_WHITE,
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        textStyle: const TextStyle(
+          fontSize: 14,
+          fontFamily: 'SF Pro Display',
+          fontWeight: FontWeight.bold,
         ),
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          padding: const EdgeInsets.all(15),
-          textStyle: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
-            fontFamily: 'SF Pro Display',
-          ),
-          shadowColor: Colors.transparent,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
         ),
+        padding: const EdgeInsets.all(15),
+        textStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          fontFamily: 'SF Pro Display',
+        ),
+        shadowColor: Colors.transparent,
       ),
-      appBarTheme: const AppBarTheme(
-        centerTitle: true,
-        iconTheme: IconThemeData(color: COLOR_ALMOST_BLACK),
-        foregroundColor: COLOR_ALMOST_BLACK,
-        backgroundColor: COLOR_WHITE,
-        elevation: 0,
-      ));
+    ),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      iconTheme: IconThemeData(color: COLOR_ALMOST_BLACK),
+      foregroundColor: COLOR_ALMOST_BLACK,
+      backgroundColor: COLOR_WHITE,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontSize: 36,
+        fontWeight: FontWeight.bold,
+        color: COLOR_ALMOST_BLACK,
+        height: 1.2,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+        color: COLOR_ALMOST_BLACK,
+      ),
+      headlineSmall: TextStyle(
+        fontSize: 21,
+        fontWeight: FontWeight.bold,
+        color: COLOR_ALMOST_BLACK,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+        color: COLOR_ALMOST_BLACK,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 14,
+        color: COLOR_ALMOST_BLACK,
+      ),
+    ),
+  );
 }
 
 const Color COLOR_WHITE = Color(0xffffffff);
 const Color COLOR_BLACK = Color(0xff000000);
 const Color COLOR_ALMOST_BLACK = Color(0xff222222);
+const Color COLOR_LIGHT_GRAY = Color(0xffEEF0F3);
+const Color COLOR_LIGHT_GRAY2 = Color(0xffC4C4C4);
+const Color COLOR_GRAY = Color(0xff87899B);
+const Color COLOR_GREEN = Color(0xff0E9E17);
 
 const Color APP_BODY_BG = COLOR_WHITE;
