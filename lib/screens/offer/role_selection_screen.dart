126d4996f7e03859156c0359e05be0382f1a7e11
Kirill Gladkov <crosfire2011@gmail.com>
2023-03-21 23:15:40 +0200

    create RoleSelectionScreen

---
+++ lib/screens/offer/role_selection_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iw_app/l10n/generated/app_localizations.dart';
+import 'package:iw_app/models/organization_member_model.dart';
+import 'package:iw_app/theme/app_theme.dart';
+import 'package:iw_app/widgets/scaffold/screen_scaffold.dart';
+
+class RoleSelection extends StatefulWidget {
+  final OrganizationMember member;
+
+  const RoleSelection({Key? key, required this.member}) : super(key: key);
+
+  @override
+  State<RoleSelection> createState() => _RoleSelectionState();
+}
+
+class _RoleSelectionState extends State<RoleSelection> {
+  OrganizationMember get member => widget.member;
+  late MemberRole _role;
+
+  @override
+  void initState() {
+    super.initState();
+    _role = member.role!;
+  }
+
+  handleNext() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenScaffold(
+        title: 'Choose Role',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 10),
+            const Text(
+              'Roles below provide different levels of rights of governing organization',
+              style: TextStyle(color: COLOR_GRAY),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Role',
+              style: TextStyle(fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: const Text('Member'),
+              leading: Radio(
+                activeColor: Colors.black,
+                value: MemberRole.Member,
+                groupValue: _role,
+                onChanged: (MemberRole? role) {
+                  setState(() {
+                    _role = role!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: const Text('Admin'),
+              leading: Radio(
+                activeColor: Colors.black,
+                value: MemberRole.Admin,
+                groupValue: _role,
+                onChanged: (MemberRole? role) {
+                  setState(() {
+                    _role = role!;
+                  });
+                },
+              ),
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: const Text('Co-owner'),
+              leading: Radio(
+                activeColor: Colors.black,
+                value: MemberRole.CoOwner,
+                groupValue: _role,
+                onChanged: (MemberRole? role) {
+                  setState(() {
+                    _role = role!;
+                  });
+                },
+              ),
+            ),
+            const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 290,
+                  child: ElevatedButton(
+                    onPressed: handleNext,
+                    child: Text(AppLocalizations.of(context)!.common_next),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
