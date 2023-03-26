add2e2a18585699917e94be627f4a29e8d9ee7d0
Kirill Gladkov <crosfire2011@gmail.com>
2023-03-26 21:35:42 +0300

    add ReceiveMoneyScreen basic screen and link to it

---
+++ lib/screens/organization/receive_money_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:iw_app/widgets/scaffold/screen_scaffold.dart';
+
+class ReceiveMoneyScreen extends StatelessWidget {
+  const ReceiveMoneyScreen({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const ScreenScaffold(title: 'Item and Price', child: Text('Hey'));
+  }
+}
