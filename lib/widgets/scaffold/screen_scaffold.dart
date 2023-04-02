68cb925e6857a41793f5ac6c1a7dab368f76fa3b
Anton Pashkovskyi <anton@globalbit.co.il>
2023-04-02 23:21:20 +0200

    fix receive payment qr ui cluttering

--- lib/widgets/scaffold/screen_scaffold.dart
+++ lib/widgets/scaffold/screen_scaffold.dart
@@ -14,7 +14,7 @@ class ScreenScaffold extends StatelessWidget {
           title: Text(title),
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          padding: const EdgeInsets.symmetric(horizontal: 30),
           child: child,
         ),
       ),
