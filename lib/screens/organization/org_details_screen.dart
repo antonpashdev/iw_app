1f564ae5c3dd55de4da36b60e9fdf056f8e3417a
Anton Pashkovskyi <anton@globalbit.co.il>
2023-05-21 23:51:32 +0300

    add org wallet to org details screen

--- lib/screens/organization/org_details_screen.dart
+++ lib/screens/organization/org_details_screen.dart
@@ -543,6 +543,79 @@ class _OrgDetailsScreenState extends State<OrgDetailsScreen> {
     );
   }
 
+  callSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).size.height - 70,
+          left: 20,
+          right: 20,
+        ),
+        content: const Text('Copied to clipboard',
+            textAlign: TextAlign.center, style: TextStyle(color: Colors.white)),
+        duration: const Duration(milliseconds: 300),
+        backgroundColor: Colors.black.withOpacity(0.7),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+      ),
+    );
+  }
+
+  handleCopyPressed(String text) async {
+    Clipboard.setData(ClipboardData(text: text));
+    callSnackBar(context);
+  }
+
+  buildWalletSection(Organization org) {
+    return Column(
+      children: [
+        Center(
+          child: TextButton.icon(
+            onPressed: () => handleCopyPressed(org.wallet!),
+            style: const ButtonStyle(
+              visualDensity: VisualDensity(vertical: 1),
+            ),
+            icon: SvgPicture.asset(
+              'assets/icons/wallet.svg',
+              width: 30,
+            ),
+            label: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Solana Address',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelMedium!
+                      .copyWith(color: COLOR_GRAY, fontWeight: FontWeight.w500),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      org.wallet!.replaceRange(8, 36, '...'),
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Icon(
+                      Icons.copy,
+                      size: 12,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   handleStartContributingPressed() async {
     setState(() {
       isLoading = true;
@@ -628,7 +701,8 @@ class _OrgDetailsScreenState extends State<OrgDetailsScreen> {
                                   color: APP_BODY_BG,
                                   child: Column(
                                     children: [
-                                      const SizedBox(height: 20),
+                                      buildWalletSection(snapshot.data?[0]),
+                                      const SizedBox(height: 10),
                                       AppPadding(
                                         child: buildHeader(
                                             context, snapshot.data?[0]),
@@ -764,10 +838,10 @@ class _HeaderDelegate extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  double get maxExtent => 140;
+  double get maxExtent => 180;
 
   @override
-  double get minExtent => 140;
+  double get minExtent => 180;
 
   @override
   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
