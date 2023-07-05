d052ebbbda474ae1f4f1aebb33f81cd669e52dd9
Anton Pashkovskyi <anton@globalbit.co.il>
2023-07-05 12:05:11 +0200

    add animation to app progress bar

--- lib/widgets/components/app_progress_bar.dart
+++ lib/widgets/components/app_progress_bar.dart
@@ -3,25 +3,69 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:iw_app/theme/app_theme.dart';
 
-class AppProgressBar extends StatelessWidget {
+class AppProgressBar extends StatefulWidget {
   final double progress;
   final double size;
   final Color backgroundColor;
   final Color progressColor;
 
   const AppProgressBar({
-    super.key,
+    Key? key,
     required this.progress,
     this.size = 100.0,
     this.backgroundColor = COLOR_PURPLE2,
     this.progressColor = COLOR_PINK,
-  });
+  }) : super(key: key);
+
+  @override
+  State<AppProgressBar> createState() => _AppProgressBarState();
+}
+
+class _AppProgressBarState extends State<AppProgressBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _progress;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this,
+    );
+    _progress = CurvedAnimation(parent: _controller, curve: Curves.linear);
+    _controller.animateTo(widget.progress);
+  }
 
   @override
   Widget build(BuildContext context) {
+    return _AnimatedAppProgressBar(
+      progress: _progress,
+      size: widget.size,
+      backgroundColor: widget.backgroundColor,
+      progressColor: widget.progressColor,
+    );
+  }
+}
+
+class _AnimatedAppProgressBar extends AnimatedWidget {
+  final double size;
+  final Color backgroundColor;
+  final Color progressColor;
+
+  const _AnimatedAppProgressBar({
+    required Animation<double> progress,
+    required this.size,
+    required this.backgroundColor,
+    required this.progressColor,
+  }) : super(listenable: progress);
+
+  @override
+  Widget build(BuildContext context) {
+    final progress = listenable as Animation<double>;
     return CustomPaint(
       painter: _AppProgressBarPainter(
-        progress: progress,
+        progress: progress.value,
         backgroundColor: backgroundColor,
         progressColor: progressColor,
       ),
