fd90e4ad662f7bd7096685fd97dbccc04817fdbf
kirillgladkov <crosfire2011@gmail.com>
2023-07-06 21:54:51 +0300

    remove unused import

--- lib/screens/offer/role_selection/role_selection_screen.dart
+++ lib/screens/offer/role_selection/role_selection_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iw_app/l10n/generated/app_localizations.dart';
 import 'package:iw_app/models/offer_model.dart';
