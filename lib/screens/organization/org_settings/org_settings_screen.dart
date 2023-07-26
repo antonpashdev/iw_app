a31ff12428cc471f91ef3c6ac084eb8508f13bab
Anton Pashkovskyi <anton@globalbit.co.il>
2023-07-26 14:55:21 +0200

    remove demo text in org settings

--- lib/screens/organization/org_settings/org_settings_screen.dart
+++ lib/screens/organization/org_settings/org_settings_screen.dart
@@ -10,7 +10,6 @@ import 'package:iw_app/screens/organization/org_edit/org_edit_screen.dart';
 import 'package:iw_app/screens/organization/org_settings/api.dart';
 import 'package:iw_app/screens/organization/org_settings/builders/header.dart';
 import 'package:iw_app/screens/organization/org_settings/builders/pending_offers_list.dart';
-import 'package:iw_app/theme/app_theme.dart';
 import 'package:iw_app/widgets/buttons/gray_button.dart';
 import 'package:iw_app/widgets/components/round_border_container.dart';
 import 'package:iw_app/widgets/list/keyboard_dismissable_list.dart';
@@ -101,17 +100,11 @@ class _OrgSettingsScreenState extends State<OrgSettingsScreen> {
           buildHeader(context, widget.organization),
           const SizedBox(height: 20),
           if (widget.organization.description != null)
-            Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: COLOR_LIGHT_GRAY3,
-                borderRadius: BorderRadius.circular(20),
-              ),
+            RoundBorderContainer(
               child: Text(
                 widget.organization.description!,
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
+                style: const TextStyle(fontSize: 18),
+                softWrap: true,
               ),
             ),
           const SizedBox(height: 30),
