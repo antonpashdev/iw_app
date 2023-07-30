a93c3870f83bc6ae413f3812fc3b4320a003dca7
kirillgladkov <crosfire2011@gmail.com>
2023-07-30 23:05:20 +0300

    hole row tapable

--- lib/screens/account_settings/widgets/settings_item.dart
+++ lib/screens/account_settings/widgets/settings_item.dart
@@ -21,52 +21,55 @@ class SettingsItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return visible == false
         ? Container()
-        : Container(
-            width: double.infinity,
-            height: 50,
-            decoration: const BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: COLOR_LIGHT_GRAY2,
-                  width: 1,
+        : GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => navigateTo),
+              );
+            },
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: COLOR_LIGHT_GRAY2,
+                    width: 1,
+                  ),
                 ),
               ),
+              child: Row(children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: icon != null
+                      ? Icon(
+                          icon,
+                          color: COLOR_GRAY,
+                          size: 16,
+                        )
+                      : image,
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: COLOR_ALMOST_BLACK,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: COLOR_GRAY,
+                    size: 16,
+                  ),
+                ),
+              ]),
             ),
-            child: Row(children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: icon != null
-                    ? Icon(
-                        icon,
-                        color: COLOR_GRAY,
-                        size: 16,
-                      )
-                    : image,
-              ),
-              const SizedBox(width: 20),
-              Text(
-                title,
-                style: const TextStyle(
-                  color: COLOR_ALMOST_BLACK,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const Spacer(),
-              IconButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => navigateTo),
-                  );
-                },
-                icon: const Icon(
-                  Icons.arrow_forward_ios,
-                  color: COLOR_GRAY,
-                  size: 16,
-                ),
-              ),
-            ]),
           );
   }
 }
