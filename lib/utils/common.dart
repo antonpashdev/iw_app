1fb1e3ecb1b3d6d8c428e4c1c323b8e34b403d66
Anton Pashkovskyi <anton@globalbit.co.il>
2023-08-08 21:42:23 +0200

    fix stringToEnum function

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -1,6 +1,8 @@
 class CommonUtils {
-  static stringToEnum(String s, List values) {
+  static stringToEnum(String? s, List values) {
+    if (s == null) return null;
     return values.firstWhere(
-        (e) => e.toString().split('.')[1].toUpperCase() == s.toUpperCase(),);
+      (e) => e.toString().split('.')[1].toUpperCase() == s.toUpperCase(),
+    );
   }
 }
