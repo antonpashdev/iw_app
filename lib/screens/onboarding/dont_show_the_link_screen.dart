3773ee1c9037de6c51666835e2319a590357e61a
kirillgladkov <crosfire2011@gmail.com>
2023-08-11 22:39:19 +0300

    change onboarding text

--- lib/screens/onboarding/dont_show_the_link_screen.dart
+++ lib/screens/onboarding/dont_show_the_link_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iw_app/l10n/generated/app_localizations.dart';
 import 'package:iw_app/screens/onboarding/check_login_link_screen.dart';
 import 'package:iw_app/widgets/scaffold/screen_scaffold.dart';
 
@@ -9,52 +8,76 @@ class DontShowTheLinkScreen extends StatelessWidget {
   const DontShowTheLinkScreen({Key? key, required this.link}) : super(key: key);
 
   handleNext(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return CheckLoginLinkScreen(
-        link: link,
-      );
-    },),);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return CheckLoginLinkScreen(
+            link: link,
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     const TextStyle textStyle = TextStyle(
-        fontSize: 18, color: Color(0xFFBB3A79), fontWeight: FontWeight.w700,);
+      fontSize: 18,
+      color: Color(0xFFBB3A79),
+      fontWeight: FontWeight.w700,
+    );
 
     return ScreenScaffold(
-        title: AppLocalizations.of(context)!.dontShowTheLinkScreen_title,
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const SizedBox(height: 80),
-              Text(
-                AppLocalizations.of(context)!.dontShowTheLinkScreen_description,
-                style: textStyle,
-                textAlign: TextAlign.center,
+      title: 'Don’t Show the Link',
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const SizedBox(height: 80),
+          const Text(
+            "Don't show the Link to Your'\nAccount to anyone else.",
+            style: textStyle,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30),
+          const Text(
+            "Don't send the Link to Your\nAccount to anyone else.",
+            style: textStyle,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 30),
+          RichText(
+            textAlign: TextAlign.center,
+            text: const TextSpan(
+              style: textStyle,
+              children: [
+                TextSpan(
+                  text: "Don't put this link to any\nwebsites except\n",
+                ),
+                TextSpan(
+                  text: 'app.equitywallet.org',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 45),
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 270,
+              child: ElevatedButton(
+                onPressed: () => handleNext(context),
+                child: const Text(
+                  'Got it. Continue.',
+                ),
               ),
-              const SizedBox(height: 30),
-              Text(
-                AppLocalizations.of(context)!
-                    .dontShowTheLinkScreen_description2,
-                style: textStyle,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 30),
-              Text(
-                AppLocalizations.of(context)!
-                    .dontShowTheLinkScreen_description3,
-                style: textStyle,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 45),
-              Align(
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                      width: 270,
-                      child: ElevatedButton(
-                          onPressed: () => handleNext(context),
-                          child: Text(AppLocalizations.of(context)!
-                              .dontShowTheLinkScreen_gotIt__button_text,),),),),
-            ],),);
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/onboarding/login_link_screen.dart
+++ lib/screens/onboarding/login_link_screen.dart
@@ -4,7 +4,6 @@ import 'package:iw_app/screens/onboarding/dont_show_the_link_screen.dart';
 import 'package:iw_app/widgets/buttons/secondary_button.dart';
 import 'package:iw_app/widgets/form/input_form.dart';
 import 'package:iw_app/widgets/scaffold/screen_scaffold.dart';
-import 'package:iw_app/l10n/generated/app_localizations.dart';
 
 class LoginLinkScreen extends StatefulWidget {
   final String link;
@@ -20,100 +19,118 @@ class _LoginLinkScreen extends State<LoginLinkScreen> {
   bool _copied = false;
 
   handleNext() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return DontShowTheLinkScreen(
-        link: link,
-      );
-    },),);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return DontShowTheLinkScreen(
+            link: link,
+          );
+        },
+      ),
+    );
   }
 
   callSnackBar(BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
         behavior: SnackBarBehavior.floating,
         margin: EdgeInsets.only(
           bottom: MediaQuery.of(context).size.height - 70,
           left: 20,
           right: 20,
         ),
-        content: Text(AppLocalizations.of(context)!.common_link_copied,
-            textAlign: TextAlign.center,
-            style: const TextStyle(color: Colors.white),),
+        content: const Text(
+          'Link copied',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white),
+        ),
         duration: const Duration(milliseconds: 300),
         backgroundColor: Colors.black.withOpacity(0.7),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
-        ),),);
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return ScreenScaffold(
-        title: AppLocalizations.of(context)!.linkIsYourLoginScreen_title,
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Text(
-                    AppLocalizations.of(context)!
-                        .linkIsYourLoginScreen_description1,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                        fontSize: 18,
-                        color: Color(0xFFBB3A79),
-                        fontWeight: FontWeight.w700,),
-                  ),),
-              const SizedBox(height: 20),
-              RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                      style: const TextStyle(
-                          fontSize: 18,
-                          color: Color(0xFFBB3A79),
-                          fontWeight: FontWeight.w700,),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: AppLocalizations.of(context)!
-                                .linkIsYourLoginScreen_description_2_1,),
-                        TextSpan(
-                            text: AppLocalizations.of(context)!
-                                .linkIsYourLoginScreen_description_2_2,
-                            style: const TextStyle(color: Color(0xFF000000)),),
-                        TextSpan(
-                            text: AppLocalizations.of(context)!
-                                .linkIsYourLoginScreen_description_2_3,),
-                      ],),),
-              const SizedBox(height: 20),
-              AppTextFormFieldBordered(
-                enabled: false,
-                readOnly: true,
-                minLines: 8,
-                maxLines: 8,
-                controller: TextEditingController(text: link),
+      title: 'Link is your login',
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                'This unique link is a login to your account.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xFFBB3A79),
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-              const SizedBox(height: 40),
-              Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    SecondaryButton(
-                      onPressed: () {
-                        Clipboard.setData(ClipboardData(text: link));
-                        callSnackBar(context);
-                        setState(() {
-                          _copied = true;
-                        });
-                      },
-                      child: Text(
-                          AppLocalizations.of(context)!.common_copy_the_link,),
-                    ),
-                    const SizedBox(height: 10),
-                    ElevatedButton(
-                        onPressed: _copied ? handleNext : null,
-                        child: Text(AppLocalizations.of(context)!.common_next),)
-                  ],)
-            ],
-          ),
-        ),);
+            ),
+            const SizedBox(height: 20),
+            RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xFFBB3A79),
+                  fontWeight: FontWeight.w700,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: 'Please, ',
+                  ),
+                  TextSpan(
+                    text: 'copy the link and save it in the safe place ',
+                    style: TextStyle(color: Color(0xFF000000)),
+                  ),
+                  TextSpan(
+                    text: 'to be able to log in to your account.',
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            AppTextFormFieldBordered(
+              enabled: false,
+              readOnly: true,
+              minLines: 8,
+              maxLines: 8,
+              controller: TextEditingController(text: link),
+            ),
+            const SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SecondaryButton(
+                  onPressed: () {
+                    Clipboard.setData(ClipboardData(text: link));
+                    callSnackBar(context);
+                    setState(() {
+                      _copied = true;
+                    });
+                  },
+                  child: const Text(
+                    'Copy the Link',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _copied ? handleNext : null,
+                  child: const Text('Next'),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
