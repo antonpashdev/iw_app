f43b6f01512947fcc5ccd647f65ff013d4f4584d
kirillgladkov <crosfire2011@gmail.com>
2023-08-18 22:34:50 +0300

    make bottom sheet supports pre and post elements

--- lib/widgets/components/bottom_sheet_custom.dart
+++ lib/widgets/components/bottom_sheet_custom.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class _CustomBottomSheet extends StatelessWidget {
-  final String title;
+  final String? title;
   final Widget child;
+  final Widget? left;
+  final Widget? right;
 
   const _CustomBottomSheet({
     Key? key,
-    required this.title,
+    this.title,
+    this.left,
+    this.right,
     required this.child,
   }) : super(key: key);
 
@@ -25,15 +29,26 @@ class _CustomBottomSheet extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(35, 35, 35, 50),
           child: Column(
             mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Color(0xFF222222),
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.w700,
-                ),
+              Row(
+                children: [
+                  left ?? const Text(''),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        title ?? '',
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Color(0xFF222222),
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                  right ?? const Text(''),
+                ],
               ),
               const SizedBox(height: 20),
               Container(child: child),
@@ -48,15 +63,20 @@ class _CustomBottomSheet extends StatelessWidget {
 // define function to show modal bottom sheet
 showBottomSheetCustom(
   BuildContext context, {
-  required String title,
   required Widget child,
+  String? title,
+  Widget? left,
+  Widget? right,
 }) {
   return showModalBottomSheet(
     context: context,
     backgroundColor: Colors.transparent,
+    isScrollControlled: true,
     builder: (BuildContext context) {
       return _CustomBottomSheet(
         title: title,
+        left: left,
+        right: right,
         child: child,
       );
     },
